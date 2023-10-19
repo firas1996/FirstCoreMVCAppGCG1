9e3c33e57dc133f891278d27dacae6cfb7dcce6c
firas <firas@DESKTOP-FAGIABL>
2023-10-19 12:35:44 +0200

    Ajoutez des fichiers projet.

---
+++ FirstCoreMVCAppGCG1/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCoreMVCAppGCG1.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Welcome()
+        {
+            return "Welcome to my First ASP.NET Core MVC Application";
+        }
+        public IActionResult WelcomeX(string name)
+        {
+            ViewBag.msg = name + ", " + "Welcome to my First ASP.NET Core MVC Application";
+            return View();
+        }
+        public IActionResult Max(int x, int y)
+        {
+            ViewBag.maxVal = Math.Max(x,y);
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstCoreMVCAppGCG1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstCoreMVCAppGCG1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
